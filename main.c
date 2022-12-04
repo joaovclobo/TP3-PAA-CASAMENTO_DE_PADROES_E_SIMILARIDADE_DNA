1e6bfad369d543651891f6a0c70d2bf5bdaa97ad
joaovclobo <joao.lobo@ufv.br>
2022-12-04 19:46:13 -0300

    Organização código

--- main.c
+++ main.c
@@ -15,96 +15,64 @@ int main(){
     time_t tempo;
     srand((time(&tempo)));
 
-        srand(1);
-
     //Variaveis de teste
-        char* DNA = (char*) calloc(MAX_BUFFER, sizeof(char));
-                char* padrao = (char*) calloc(MIN_BUFFER, sizeof(char));
-
-        int linhasLidas = 0;
-
+        srand(1);
         tamPadrao = 2; numPadroes = 4;
     
     cabecalhoMain();
 
     //TODO - da pra melhorar
+
+    /*------------------------------- Gera Produto Cartesiano -------------------------------*/
     tamProdCartesiano = pow(4, tamPadrao);
 	char** prodCartesiano = geraProdCartesiano(tamPadrao, tamProdCartesiano);
 
     printf("\n\tProduto cartesiano:\t");
     imprimeVetString(tamProdCartesiano, tamPadrao + 1, prodCartesiano);
 
+
+    /*------------------------------- Sorteia Padrões -------------------------------*/
     char** padroesSorteados = sorteiaPadroes(numPadroes, tamPadrao + 1, tamProdCartesiano, prodCartesiano);
 
     printf("\n\tPadrões sorteados:\t");
     imprimeVetString(numPadroes, tamPadrao + 1, padroesSorteados);
 
-    begin = clock();
-    
+    /*------------------------------- Calcula Similaridades -------------------------------*/
 
+    /*----  Calcula Frequencias Humano ----*/
+    begin = clock();
     FILE *fptrHuman = abreArquivo("./ArquivosDNA/human.txt");
     long* freqsHuman = contaFrequenciasBHMS(fptrHuman, "Humano", numPadroes, padroesSorteados);
+    end = clock();
 
     printf("\tVetor freqs Human: "); imprimeVetLong(numPadroes, freqsHuman); 
+    mostraTempoGasto(end, begin);
 
 
+    /*----  Calcula Frequencias Cachoro ----*/
+    begin = clock();
     FILE *fptrDog = abreArquivo("./ArquivosDNA/dog.txt");
     long* freqsDog = contaFrequenciasBHMS(fptrDog, "Cachoro", numPadroes, padroesSorteados);
+    end = clock();
 
     printf("\tVetor freqs Dog: "); imprimeVetLong(numPadroes, freqsDog); 
+    mostraTempoGasto(end, begin);
 
-
+    
+    /*----  Calcula Frequencias Chimpanzé ----*/
+    begin = clock();
     FILE *fptrChimp = abreArquivo("./ArquivosDNA/chimpanzee.txt");
     long* freqsChimp = contaFrequenciasBHMS(fptrChimp, "Chimpanzé", numPadroes, padroesSorteados);
+    end = clock();
 
     printf("\tVetor freqs Chimpanzee: "); imprimeVetLong(numPadroes, freqsChimp); 
-
-    end = clock();
-
     mostraTempoGasto(end, begin);
 
-    // printf("LINHAS LIDAS %d\n", linhasLidas); linhasLidas = 0;
 
-/*     
-    cabecalhoMain();
-
-    begin = clock();
-
-        while (!feof(fptrHuman)){fscanf(fptrHuman, "%s", DNA);linhasLidas++;}
-    
-    end = clock();
-
-    mostraTempoGasto(end, begin);
-    
-    printf("LINHAS LIDAS %d\n", linhasLidas); linhasLidas = 0;
-
-    begin = clock(); 
-
-        while (!feof(fptrChimp)){fscanf(fptrChimp, "%s", DNA);linhasLidas++;}
-    
-    end = clock();
-
-    mostraTempoGasto(end, begin);
-    printf("LINHAS LIDAS %d\n", linhasLidas); linhasLidas = 0; 
-
-
-    begin = clock(); 
-
-        for (int i = 0; i < 100000000; i++){
-            while (!feof(fptrDog)){fscanf(fptrDog, "%s", DNA); printf("%s", DNA); linhasLidas++;}
-            
-        }
-
-    end = clock();
-
-    mostraTempoGasto(end, begin);
-    printf("LINHAS LIDAS %d\n", linhasLidas); linhasLidas = 0;     
-*/
-    
+    /*------------------------------- Fecha os arquivos -------------------------------*/
     fclose(fptrHuman);
     fclose(fptrChimp);
     fclose(fptrDog);
 
     return 0;
 }
-
